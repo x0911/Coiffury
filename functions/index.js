c9ea12587b35f8799a2d587f24904f9c69bde508
Hamdi Mohamed <maracap.x911@gmail.com>
2020-06-20 20:06:36 +0200

    update functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
-// Your own functions
+const functions = require('firebase-functions');
 
-function sayHi(name) {
-  return "Hi " + name;
-}
-
-sayHi("moahmed");
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
